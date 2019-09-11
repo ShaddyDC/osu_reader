7dee49a74da5528c7beaf9e8edb9337199ff3d58
ShaddyAQN <ShaddyAQN@gmail.com>
2019-09-11 14:49:14 +0200

    Move parse_value definitions to new file

--- src/beatmap.cpp
+++ src/beatmap.cpp
@@ -4,89 +4,11 @@
 #include "util.h"
 #include <map>
 #include <variant>
+#include "parse_string.h"
 
 using Beatmap_types = std::variant<int*, float*, bool*, std::string*,
                                    std::chrono::milliseconds*, std::filesystem::path*, std::uint8_t*, Gamemode*,
                                    std::vector<std::chrono::milliseconds>*, std::vector<std::string>*>;
-
-
-template<typename Type>
-void parse_value(std::string_view value_string, Type& value)
-{
-	std::from_chars(value_string.data(), value_string.data() + value_string.length(), value);
-}
-
-template<>
-void parse_value<>(std::string_view value_string, bool& value)
-{
-	if(!value_string.empty()){
-		if(value_string[0] == '1') value = true;
-		else if(value_string[0] == '0') value = false;
-	}
-}
-
-template<>
-void parse_value<>(std::string_view value_string, std::chrono::milliseconds& value)
-{
-	auto v = 0;
-	parse_value(value_string, v);
-	value = std::chrono::milliseconds{ v };
-}
-
-template<>
-void parse_value<>(std::string_view value_string, Gamemode& value)
-{
-	auto v = 0;
-	parse_value(value_string, v);
-	value = static_cast<Gamemode>(value);
-}
-
-template<>
-void parse_value<>(std::string_view value_string, std::string& value)
-{
-	value = value_string;
-}
-
-template<>
-void parse_value<>(std::string_view value_string, std::filesystem::path& value)
-{
-	std::string s;
-	parse_value(value_string, s);
-	value = s;
-}
-
-template<>
-void parse_value<>(std::string_view value_string, std::vector<std::string>& value)
-{
-	const auto tokens = split(value_string, ' ');
-	std::transform(tokens.cbegin(), tokens.cend(), std::back_inserter(value),
-	               [](const auto e)
-	               {
-		               return std::string{ e };
-	               });
-}
-
-template<>
-void parse_value<>(std::string_view value_string, std::vector<std::chrono::milliseconds>& value)
-{
-	const auto tokens = split(value_string, ' ');
-	std::transform(tokens.cbegin(), tokens.cend(), std::back_inserter(value),
-	               [](const std::string_view s)
-	               {
-		               const auto trimmed = ltrim_view(s);
-		               int v;
-		               std::from_chars(trimmed.data(), trimmed.data() + trimmed.length(), v);
-		               return std::chrono::milliseconds{ v };
-	               });
-}
-
-template<typename Type>
-Type parse_value(std::string_view value_string)
-{
-	Type value{};
-	parse_value(value_string, value);
-	return value;
-}
 
 std::optional<osu::Beatmap_file> osu::Beatmap_parser::parse(const std::filesystem::path& file)
 {
---
+++ src/parse_string.h
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <string_view>
+#include <chrono>
+#include "osu_reader/beatmap.h"
+#include "string_stuff.h"
+#include <filesystem>
+#include <vector>
+
+template<typename Type>
+void parse_value(std::string_view value_string, Type& value)
+{
+	std::from_chars(value_string.data(), value_string.data() + value_string.length(), value);
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, bool& value)
+{
+	if (!value_string.empty()) {
+		if (value_string[0] == '1') value = true;
+		else if (value_string[0] == '0') value = false;
+	}
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, std::chrono::milliseconds& value)
+{
+	auto v = 0;
+	parse_value(value_string, v);
+	value = std::chrono::milliseconds{ v };
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, Gamemode& value)
+{
+	auto v = 0;
+	parse_value(value_string, v);
+	value = static_cast<Gamemode>(value);
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, std::string& value)
+{
+	value = value_string;
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, std::filesystem::path& value)
+{
+	std::string s;
+	parse_value(value_string, s);
+	value = s;
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, std::vector<std::string>& value)
+{
+	const auto tokens = split(value_string, ' ');
+	std::transform(tokens.cbegin(), tokens.cend(), std::back_inserter(value),
+		[](const auto e)
+	{
+		return std::string{ e };
+	});
+}
+
+template<>
+inline void parse_value<>(std::string_view value_string, std::vector<std::chrono::milliseconds>& value)
+{
+	const auto tokens = split(value_string, ' ');
+	std::transform(tokens.cbegin(), tokens.cend(), std::back_inserter(value),
+		[](const std::string_view s)
+	{
+		const auto trimmed = ltrim_view(s);
+		int v;
+		std::from_chars(trimmed.data(), trimmed.data() + trimmed.length(), v);
+		return std::chrono::milliseconds{ v };
+	});
+}
+
+template<typename Type>
+Type parse_value(std::string_view value_string)
+{
+	Type value{};
+	parse_value(value_string, value);
+	return value;
+}
