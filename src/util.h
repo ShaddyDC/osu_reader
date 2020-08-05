b3649453487808115e3b644014c4d4c49d71c995
ShaddyDC <shaddythefirst@gmail.com>
2020-08-05 19:10:06 +0200

    make _ch literal constexpr

--- src/util.h
+++ src/util.h
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace util{
-	inline char operator "" _ch(const unsigned long long int value)
+	inline constexpr char operator "" _ch(const unsigned long long int value)
 	{
 		return static_cast<char>(value);
 	}
