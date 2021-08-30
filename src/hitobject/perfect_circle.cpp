a39ca42051a301ef07bfaf0178cd16f26699892d
ShaddyDC <shaddythefirst@gmail.com>
2021-08-30 19:44:56 +0200

    Use optional instead of struct flag for perfect circle

--- src/hitobject/perfect_circle.cpp
+++ src/hitobject/perfect_circle.cpp
@@ -2,18 +2,20 @@
 #include "perfect_circle.h"
 #include "bezier.h"
 #include <cmath>
+#include <optional>
 #include <stack>
+
 struct Circular_properties {
-    bool is_valid = false;
     double theta_start = 0;
     double theta_range = 0;
     double direction = 0;
     float radius = 0;
     osu::Vector2 centre = {0, 0};
 };
-Circular_properties circular_properties(const std::vector<osu::Vector2>& control_points)
+
+std::optional<Circular_properties> circular_properties(const std::vector<osu::Vector2>& control_points)
 {
-    if(control_points.size() != 3) return {};
+    if(control_points.size() != 3) return std::nullopt;
 
     const auto a = control_points[0];
     const auto b = control_points[1];
@@ -21,7 +23,7 @@ Circular_properties circular_properties(const std::vector<osu::Vector2>& control
 
     // Degenerate triangle with side-length almost zero - fall back to numerically stable method
     if(std::abs((b.y - a.y) * (c.x - a.x) - (b.x - a.x) * (c.y - a.y)) <= 1e-3f)
-        return {};
+        return std::nullopt;
 
     const auto d = 2.f * (a.x * (b - c).y + b.x * (c - a).y + c.x * (a - b).y);
     const auto a_sq = length_squared(a);
@@ -52,7 +54,7 @@ Circular_properties circular_properties(const std::vector<osu::Vector2>& control
         dir = -dir;
         theta_range = static_cast<float>(2 * M_PI) - theta_range;
     }
-    return Circular_properties{true, theta_start, theta_range, dir, r, centre};
+    return Circular_properties{theta_start, theta_range, dir, r, centre};
 }
 
 std::vector<osu::Vector2> approximate_perfect(const std::vector<osu::Vector2>& control_points)
@@ -60,7 +62,7 @@ std::vector<osu::Vector2> approximate_perfect(const std::vector<osu::Vector2>& c
     constexpr const auto circular_arc_tolerance = 0.1;
 
     const auto properties = circular_properties(control_points);
-    if(!properties.is_valid) return approximate_bezier(control_points);
+    if(!properties) return approximate_bezier(control_points);
 
     // To quote peppy:
     // We select the amount of points for the approximation by requiring the discrete curvature
@@ -68,15 +70,15 @@ std::vector<osu::Vector2> approximate_perfect(const std::vector<osu::Vector2>& c
     // is: 2 * Math.Acos(1 - TOLERANCE / r)
     // The special case is required for extremely short sliders where the radius is smaller than
     // the tolerance. This is a pathological rather than a realistic case.
-    const auto point_count = 2 * properties.radius <= circular_arc_tolerance
+    const auto point_count = 2 * properties->radius <= circular_arc_tolerance
                                      ? 2
-                                     : std::max(2., std::ceil(properties.theta_range / (2 * std::acos(1 - circular_arc_tolerance / properties.radius))));
+                                     : std::max(2., std::ceil(properties->theta_range / (2 * std::acos(1 - circular_arc_tolerance / properties->radius))));
 
     std::vector<osu::Vector2> points;
     for(auto i = 0; i < point_count; ++i) {
         const auto fract = static_cast<double>(i) / (point_count - 1);
-        const auto theta = properties.theta_start + properties.direction * fract * properties.theta_range;
-        points.push_back(properties.centre + properties.radius * osu::Vector2{static_cast<float>(std::cos(theta)), static_cast<float>(std::sin(theta))});
+        const auto theta = properties->theta_start + properties->direction * fract * properties->theta_range;
+        points.push_back(properties->centre + properties->radius * osu::Vector2{static_cast<float>(std::cos(theta)), static_cast<float>(std::sin(theta))});
     }
 
     // Failure for some reason. Maybe worth logging to investigate? Same behaviour as lazer
